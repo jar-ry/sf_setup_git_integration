b78505a2b0a9ab9cdc4797f2e2f11f434b4e22c3
Jarry <36974254+jar-ry@users.noreply.github.com>
2025-06-22 23:09:37 +1000

    Update 01_set_up.sql

--- 01_set_up.sql
+++ 01_set_up.sql
@@ -1,37 +1,23 @@
--- Step 1: Switch to ACCOUNTADMIN to create the role and assign privileges
+-- Use role, warehouse and data
 USE ROLE ACCOUNTADMIN;
+USE WAREHOUSE COMPUTE_WH;
+USE DATABASE DEMO_GIT;
+USE SCHEMA PUBLIC;
 
--- Create a dedicated database and warehouse
-CREATE OR REPLACE DATABASE DEMO_GIT;
-
-CREATE SCHEMA INTEGRATIONS;
- 
-CREATE OR REPLACE SECRET my_git_secret
-  TYPE = password
-  USERNAME = 'jar-ry'
-  PASSWORD = '';
-
--- Create git integration
-create or replace api integration git_integration
-    api_provider = git_https_api
-    api_allowed_prefixes = ('https://github.com/jar-ry')
-    enabled = true
-    allowed_authentication_secrets = (my_git_secret)
-    comment='Git integration for jar-ry';
-
+-- Create git repo
 CREATE GIT REPOSITORY snowflake_notebook_git_demo 
  ORIGIN = 'https://github.com/jar-ry/sfguide-getting-started-with-snowflake-notebook-container-runtime' 
  API_INTEGRATION = 'GIT_INTEGRATION';
 
 -- Create external access to all
---  CREATE NETWORK RULE allow_all_rule
---   TYPE = 'HOST_PORT'
---   MODE= 'EGRESS'
---   VALUE_LIST = ('0.0.0.0:443','0.0.0.0:80');
+ CREATE NETWORK RULE allow_all_rule
+  TYPE = 'HOST_PORT'
+  MODE= 'EGRESS'
+  VALUE_LIST = ('0.0.0.0:443','0.0.0.0:80');
 
--- CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION allow_all_integration
---   ALLOWED_NETWORK_RULES = (allow_all_rule)
---   ENABLED = true;
+CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION allow_all_integration
+  ALLOWED_NETWORK_RULES = (allow_all_rule)
+  ENABLED = true;
 
 -- Create external access to Pip
 CREATE OR REPLACE NETWORK RULE pypi_network_rule
